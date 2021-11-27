0440f15629b7681d0b84f17c8c3c6c334c2725ed
Michael <u17023557@tuks.co.za>
2021-11-27 21:55:33 +0200

    Added an app state

---
+++ ChatBlazorApp/Client/AppState.cs
@@ -0,0 +1,22 @@
+﻿using ChatBlazorApp.Client.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBlazorApp.Client
+{
+    public class AppState
+    {
+        public string UserName { get; set; }
+
+        public Dictionary<string, int> Rooms = new Dictionary<string, int>();
+
+        public string RoomsCommaSeperated { get; set; }
+
+        public Action AppStateUpdated { get; set; }
+
+        public Room CurrentRoom { get; set; }
+    }
+}
