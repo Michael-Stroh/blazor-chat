6ce3d910f17e084d22fc7ad86ef376745cace5f0
Michael <u17023557@tuks.co.za>
2021-11-27 18:53:03 +0200

    Created chat controller and state to store chat data

---
+++ ChatBlazorApp/Shared/ChatData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBlazorApp.Shared
+{
+
+    public class ChatData
+    {
+
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
